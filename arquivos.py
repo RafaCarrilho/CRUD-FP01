21716dc0c0d5591e08fe05dc783ba453cf980f14
Pichau <rac4@cesar.school>
2025-11-23 16:38:15 -0300

    Adicionado numero de convidados, CARRALY EXECUTE A PARTIR DAÍ

--- arquivos.py
+++ arquivos.py
@@ -1,8 +1,9 @@
-def adc_arquivo(nome, tipo, local, orca, data):
+def adc_arquivo(nome, tipo, local, orca, data, convid):
     file = open("eventos.csv", "a", encoding="utf8")
     file.writelines(f"Nome: {nome}\n")
     file.writelines(f"Tipo: {tipo}\n")
     file.writelines(f"Data: {data}\n")
     file.writelines(f"Local: {local}\n")
-    file.writelines(f"Orçamento: {orca}\n\n")
+    file.writelines(f"Orçamento: {orca}\n")
+    file.writelines(f"Convidados: {convid}\n\n")
     file.close()
